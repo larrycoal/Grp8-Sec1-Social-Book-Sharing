4e9262c73f320e44731832240125483631dcfb9e
Dhura Mistry <dhuramistry@gmail.com>
2023-02-15 17:38:49 -0500

    UI for register, login, account details

--- client/src/component/login_register/login/index.jsx
+++ client/src/component/login_register/login/index.jsx
@@ -3,7 +3,35 @@ import React from 'react';
 const index = () => {
     return (
         <div>
-            login form
+            <div>
+                <h1>Book Keepers</h1>
+            </div>
+            <div class="row">
+                <h4>Trade Books from the comfort of your home</h4>
+                <p>Welcome back, please login to your account</p>
+            </div>
+            <div class="row">
+                <form>
+                    <div class="form-group row first">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control mt-1 p-3" id="username" required />
+
+                    </div>
+                    <div class="form-group row last mb-4 mt-4">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control p-3" id="password" required />
+
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-md-6">
+                        <input type="submit" value="Login" class="btn btn-block btn-primary p-4" />
+                        </div>
+                        <div className="col-md-6">
+                        <input value="Sign Up" class="btn btn-block btn-light p-4" />
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- client/src/component/login_register/register/index.jsx
+++ client/src/component/login_register/register/index.jsx
@@ -3,7 +3,63 @@ import React from 'react';
 const index = () => {
     return (
         <div>
-            register
+            <div>
+                <h1>Book Keepers</h1>
+            </div>
+            <div class="row">
+                <h4>Trade Books from the comfort of your home</h4>
+                <p>Sign up with us</p>
+            </div>
+            <div class="row">
+                <form>
+                    <div class="form-group row first">
+                        <div className="col-md-6">
+                            <label for="firstname">First Name</label>
+                            <input type="text" class="form-control mt-1 p-2" id="firstname" required />
+                        </div>
+                        <div className="col-md-6">
+                            <label for="lastname">Last Name</label>
+                            <input type="text" class="form-control mt-1 p-2" id="lastname" required />
+                        </div>
+                    </div>
+                    <div class="form-group row last mb-4 mt-4">
+                        <div className="col-md-6">
+                            <label for="gender">Gender</label>
+                            <input type="text" class="form-control mt-1 p-2" id="gender" required />
+                        </div>
+                        <div className="col-md-6">
+                            <label for="dob">Date of Birth</label>
+                            <input type="date" class="form-control mt-1 p-2" id="dob" required />
+                        </div>
+                    </div>
+                    <div class="form-group row last mb-4 mt-4">
+                        <div className="col-md-6">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control mt-1 p-2" id="username" required />
+                        </div>
+                        <div className="col-md-6">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control mt-1 p-2" id="email" required />
+                        </div>
+                    </div>
+                    <div class="form-group row last mb-4 mt-4">
+                        <div className="col-md-6">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control p-2" id="password" required />
+                        </div>
+                        <div className="col-md-6">
+                            <label for="password">Retype Password</label>
+                            <input type="password" class="form-control p-2" id="password" required />
+                        </div>
+                        <a>Already have an account ?</a>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-md-6">
+                            <input value="Sign Up" class="btn btn-block btn-light p-4" />
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
