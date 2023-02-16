f7a7330c507335f0d676fbe36312300d56e514b5
Olayiwola Olanrewaju <larry_coal@outlook.com>
2023-02-16 12:55:44 -0500

    integrate register endpoint and login endpoint

--- server/controller/registerUserController.js
+++ server/controller/registerUserController.js
@@ -4,34 +4,36 @@ module.exports = async (req, res) => {
   const {
     email,
     password,
-    confirmpassword,
-    address,
+    province,
+    city,
+    postalCode,
     phonenumber,
     gender,
-    age,
+    dob,
     firstname,
     lastname,
   } = req.body;
   try {
-    if (password === confirmpassword) {
-      let newUser = {
-        email,
-        firstName:firstname,
-        lastName:lastname,
-        address,
-        password,
-        phonenumber,
-        gender,
-        age
-      };
-      await Users.create(newUser);
-      res.status(200).send("succesful");
-    } else {
-      console.log("registerPasswordError", "Password do not match");
-      res.status(403).send("Password do not match");
+    const existingEmail = await Users.findOne({ email });
+    if (existingEmail) {
+      return res.status(401).send("User with this email already exist");
     }
+    let newUser = {
+      email,
+      firstName: firstname,
+      lastName: lastname,
+      password,
+      phonenumber,
+      gender,
+      dob,
+      province,
+      city,
+      postalCode,
+    };
+    await Users.create(newUser);
+    return res.status(200).send("succesful");
   } catch (err) {
     console.log(err);
-    res.status(400).send(err);
+    // res.status(400).send(err);
   }
 };
